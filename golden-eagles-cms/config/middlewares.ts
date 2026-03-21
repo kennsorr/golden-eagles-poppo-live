aecaa59cf3dc5dcac4f40dedad679c636b805295
Kenneth Sorrell <kennsorr@gmail.com>
2026-03-21 19:05:01 -0400

    Add Cloudinary upload provider to Strapi configuration; update package dependencies for enhanced media management capabilities.

--- golden-eagles-cms/config/middlewares.ts
+++ golden-eagles-cms/config/middlewares.ts
@@ -1,7 +1,19 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
--- golden-eagles-cms/config/plugins.ts
+++ golden-eagles-cms/config/plugins.ts
@@ -8,4 +8,18 @@ export default ({ env }: { env: EnvFn }) => ({
       jwtSecret: env("JWT_SECRET"),
     },
   },
+  upload: {
+    config: {
+      provider: "@strapi/provider-upload-cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
 });
